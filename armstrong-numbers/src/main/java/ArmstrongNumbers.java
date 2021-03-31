48df86cb299f0d101023089583373c2a56dffd6c
Donelle Harris <donelle.harris@gmail.com>
2021-03-30 23:35:38 -0500

    Continue progress on completion of ArmstronNumbers
    Passes 4 of 9 tests

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,13 @@
 class ArmstrongNumbers {
 
     boolean isArmstrongNumber(int numberToCheck) {
-
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-
+        char[] number = (numberToCheck + "").toCharArray();
+        int sum = 0;
+        for (int i = 0; i < number.length; i++) {
+            int num = number[i];
+            sum += Math.pow(num, number.length);
+        }
+        return numberToCheck == sum;
     }
 
 }
